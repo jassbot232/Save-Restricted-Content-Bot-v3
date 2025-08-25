f7bcb0d60754fb5c46b1de164210867d7fc9ed94
jassbot232 <jashanbeniwal2006@gmail.com>
2025-08-25 07:51:44 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,14 +24,15 @@ MONGO_DB = os.getenv("MONGO_DB", "mongodb+srv://rs92573993688:pVf4EeDuRi2o92ex@c
 OWNER_ID = list(map(int, os.getenv("OWNER_ID", "1955406483").split())) # list seperated via space
 DB_NAME = os.getenv("DB_NAME", "telegram_downloader")
 STRING = os.getenv("STRING", None) # optional
-LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002493388222")) # optional with -100
-FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002575593624")) # optional with -100
+LOG_GROUP = int(os.getenv("LOG_GROUP", "-1002657252858")) # optional with -100
+FORCE_SUB = int(os.getenv("FORCE_SUB", "-1002888391802")) # optional with -100
 MASTER_KEY = os.getenv("MASTER_KEY", "gK8HzLfT9QpViJcYeB5wRa3DmN7P2xUq") # for session encryption
 IV_KEY = os.getenv("IV_KEY", "s7Yx5CpVmE3F") # for decryption
 YT_COOKIES = os.getenv("YT_COOKIES", YTUB_COOKIES)
 INSTA_COOKIES = os.getenv("INSTA_COOKIES", INST_COOKIES)
 FREEMIUM_LIMIT = int(os.getenv("FREEMIUM_LIMIT", "0"))
-PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "500"))
+PREMIUM_LIMIT = int(os.getenv("PREMIUM_LIMIT", "5000"))
 JOIN_LINK = os.getenv("JOIN_LINK", "https://t.me/+Ppr5dY6fYWU1Njc1") # this link for start command message
-ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", "https://t.me/Saverestrictedcontentbot1_bot")
+ADMIN_CONTACT = os.getenv("ADMIN_CONTACT", ""https://t.me/DRMDOWNLOADBOT01_bot)
+
 
